559259b31289edbd23d9b78fc54b74ad0a3db18c
Felipe Costa <felipeiasi@gmail.com>
2019-03-05 16:31:10 -0300

    Update connection

--- src/data/mongo_setup.py
+++ src/data/mongo_setup.py
@@ -2,6 +2,6 @@ import mongoengine
 
 
 def global_init():
-    mongoengine.register_connection(alias='core', name='rss', host='mongodb+srv://felipe:a1b2c3d4e5@cluster0-twctd.mongodb.net/rss?retryWrites=true')
+    mongoengine.register_connection(alias='core', name='rss', host='mongodb+srv://felipe:a1b2c3d4e5@cluster0-twctd.mongodb.net/news?retryWrites=true')
 
     #yvgYrHnph2DYiT35
