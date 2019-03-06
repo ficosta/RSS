7475fa2ffc15ddb8ff24ece36a249599771d006f
Felipe Costa <felipeiasi@gmail.com>
2019-03-06 10:42:17 -0300

    Monogo Local / Remoto

--- src/data/mongo_setup.py
+++ src/data/mongo_setup.py
@@ -2,6 +2,8 @@ import mongoengine
 
 
 def global_init():
-    mongoengine.register_connection(alias='core', name='rss', host='mongodb+srv://felipe:a1b2c3d4e5@cluster0-twctd.mongodb.net/news?retryWrites=true')
+    mongoengine.register_connection(alias='core', name='rss',
+                host='mongodb+srv://felipe:a1b2c3d4e5@cluster0-twctd.mongodb.net/news?retryWrites=true')
+    #Local mongoengine.register_connection(alias='core', name='rss')
 
     #yvgYrHnph2DYiT35
